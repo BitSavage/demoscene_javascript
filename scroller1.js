d5f397d7c53b8b152b8e03b0ed3dc36f4702d566
BitSavage <github@bitsavage.com>
2013-12-31 23:04:40 -0800

    First scroller version

--- scroller1.js
+++ scroller1.js
@@ -3,66 +3,95 @@ var ctx = canvas.getContext("2d");
 
 var timer;
 
-var numCircles = 765;
-var amplitude = 100;
-var frequency = 0.02;
-var shiftStartingAngle = 0;
-var shiftWaveY = 400;
-var dx = 2;
+var message = "BitSavage super simple scroller.";
 
-var startX = 5;
-var startY = 5;
+var fontSheetImage = new Image();
 
-var image = new Image();
-var input;
-var output;
+var spriteArray = new Array();
 
-var minColor = 0;
-var maxColor = 255;
-var stepColor = 0;
-
-var fontArray = new Array();
-
-var _x = 5;
-
-
-
-function Font(val, image) {
-	this.val = val;
-	this.image = new Image();
-	this.image = image;
+function spriteLoc (options) {
+	var that = {};
+	
+	that.width = options.width;
+	that.height = options.height;
+	that.sx = options.sx;
+	that.sy = options.sy;
+	that.dx = options.dx;
+	that.dy = options.dy;
+	
+	return that;
 }
 
 function init() {
-	
-	image.src = "sprite_font.png";
+
+	fontSheetImage.src = "sprite_font2.png";
 		
 	// Setup canvas
-	canvas = document.getElementById("theCanvas");
-	ctx = canvas.getContext("2d");
-	ctx.drawImage(image, 0, 0);
-	output = createImageData(canvas.width, canvas.height);	
-	output.data = ctx.getImageData(0, 0, 42, 53);
-	output.data = input.data;
+	//canvas = document.getElementById("theCanvas");
+	canvas.width = 500;
+	canvas.height = 500;
 	
-	/*
-	 *	 for(var i=33; i < 33+26; i++)
-	 {
-	 startX = ((i%10)-1)*42;
-	 startY = (i/10)*42;
-	 ctx.getImageData( startX, startY, 42, 53);
+	var cropX = 0;
+	var cropY = 0;
+	var startX = 20;//100; // 20 + (2 * 40)
+	var startY = 20; //170; // 20 + (3 * 51)
+	var xChar = 0;
+	var yChar = 0;
 
-	 } 
-	 */
 	
-	// call draw function every 10 msecs
-	timer = setInterval(draw, 10);
-	return timer;
+	for(var i = 0; i < 100; i++) {
+		spriteArray[i] = new spriteLoc({
+			width: 40,
+			height: 51,
+			sx: startX + (xChar * 40),
+			sy: startY + (yChar * 51),
+			dx: canvas.width,
+			dy: canvas.height/2,
+		});
+		
+		if(xChar >= 9) {
+			xChar = 0;
+			yChar++;
+		}
+		else xChar++;
+	}
+	
+	for(var i = 0; i < message.length; i++) {
+		charLoc[i] = canvas.width + (i * 20);		
+	}
+	
+   // call draw function every 10 msecs
+   timer = setInterval(loop, 10);
+   return timer;
+
 }
 
-function draw() {
+var charLoc = Array();
+
+function loop() {
 	ctx.clearRect(0,0,canvas.width,canvas.height);
-	ctx.putImageData(output, 110, 110);
+	
+	var asciiVal = 0;
+	
+	for(var i = 0; i < message.length; i++) {
+		
+		asciiVal = message.charCodeAt(i) - 33;
+		if (asciiVal >= 0) {
+			ctx.drawImage(
+				fontSheetImage, 
+				spriteArray[asciiVal].sx, 
+				spriteArray[asciiVal].sy, 
+				spriteArray[asciiVal].width, 
+				spriteArray[asciiVal].height, 
+				charLoc[i], 
+				spriteArray[asciiVal].dy, 
+				spriteArray[asciiVal].width, 
+				spriteArray[asciiVal].height);
+		}
+			
+		charLoc[i]--;
+		 
+	}
 }
 
 init();
